41b7940a75c4187e5299aedfa058baf322951321
nperaya <pink.nongpink2547@gmail.com>
2022-12-09 17:21:25 +0700

    lab6_2.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+    int x=1
+    int even=0, odd=0;
+    while(x!=0)
+{
+cout << "Enter an integer: ";
+cin >> x;
+if(x%2==0){
+    even++;
+    }
+else{
+    odd++;
+}
+}
+
+    
+   
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
     return 0;
